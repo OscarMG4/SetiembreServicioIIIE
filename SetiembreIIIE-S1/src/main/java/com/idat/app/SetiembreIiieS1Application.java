fbf4f5d0512156689cb7aa585add00c628f49d61
Oscar Fernando Muñoz Gayoso <oscar4tecno@gmail.com>
2022-09-13 21:08:25 -0500

    Actualizacion1

---
+++ SetiembreIIIE-S1/src/main/java/com/idat/app/SetiembreIiieS1Application.java
@@ -0,0 +1,13 @@
+package com.idat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetiembreIiieS1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetiembreIiieS1Application.class, args);
+	}
+
+}
